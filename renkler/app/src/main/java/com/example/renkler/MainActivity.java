cd1b3835797fe35eb572f04062183bb73d81d8d2
ahmetakifiron-wq <ahmetakifiron@gmail.com>
2026-04-28 10:54:25 +0300

    renk projesi eklendi

---
+++ renkler/app/src/main/java/com/example/renkler/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.renkler;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public int checkCallingOrSelfPermission(String permission) {
+        return super.checkCallingOrSelfPermission(permission);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        TextView text1=(TextView) findViewById(R.id.TextView);
+
+    }
+}
